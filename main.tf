64e0114a38aca2c8cbb83d5a4b16027538d760f5
Tod Lazarov <6321436+thermobee@users.noreply.github.com>
2024-01-23 14:17:31 -0500

    Switched the state lock to an S3 bucket

--- main.tf
+++ main.tf
@@ -403,3 +403,47 @@ module "autoscaling" {
     Name = "Web EC2 Server 2"
   }
 }
+
+output "asg_group_size" {
+  value = module.autoscaling.autoscaling_group_max_size
+}
+
+module "s3-bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "3.15.1"
+}
+
+output "s3_bucket_name" {
+  value = module.s3-bucket.s3_bucket_bucket_domain_name
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.2.0"
+
+  name = "my-vpc-terraform"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Name        = "VPC from Module"
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+#State testing
+resource "aws_instance" "web_server_2" {
+  ami           = data.aws_ami.ubuntu_22_04.id
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public_subnets["public_subnet_2"].id
+  tags = {
+    Name = "Web EC2 Server 222"
+  }
+}
--- terraform.tf
+++ terraform.tf
@@ -1,4 +1,9 @@
 terraform {
+  backend "s3" {
+    bucket = "my-terraform-state-tl"
+    key    = "prod/aws_infra"
+    region = "us-east-1"
+  }
   required_version = ">=1.0.0"
   required_providers {
     aws = {
